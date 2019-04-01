530663423e8ffc1d39124dcf3a2f8dd1ba9178e2
Meysam <esmaeilpour.meysam@gmail.com>
2019-04-01 17:14:34 +0200

    Update multiplication.f90

--- CH3/multiplication.f90
+++ CH3/multiplication.f90
@@ -1,7 +1,9 @@
 ! subroutine of matrix inner product with fortran90
+!Arguments are matrices, row, and column 
 subroutine multiplication(a, b, c, nrow, ncol)
   implicit none
   integer nrow, ncol, i, j
   double precision a(nrow, ncol), b(nrow, ncol), c(nrow, ncol)
+  !Fortran function for multiplying two matrices
   c = matmul(a, b)
 end subroutine multiplication
